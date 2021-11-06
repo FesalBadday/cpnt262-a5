fd13ab57e8a0ae374e6b5e763b4395d526396cb8
FesalBadday <fesal.n04@hotmail.com>
2021-11-06 17:42:42 -0600

    added local module

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,15 @@ const express = require('express') // import express module
 const router = express.Router()
 
 const Gallery = require('../models/gallery') // import gallery module
+const localGallery = require('../models/seeds/gallery') // import gallery module
 const randomCar = require('./random-car') // import random-car module
 
 router.get('/cars', (req, res) => {
   try {
     Gallery.find((err, data) => { // find all cars
+      if (data.length === 0) { // check if mongoose model is inoperable or non-existent
+        data = localGallery // get local module
+      }
       if (typeof data !== 'undefined' && Array.isArray(data)) { // check if data isn't undefined and it's an array
         if (req.query.filter === 'random') { // check if filter = random 
           res.send(randomCar(data)) // show the random car
@@ -29,6 +33,9 @@ router.get('/cars', (req, res) => {
 router.get('/cars/:id', (req, res) => {
   try {
     Gallery.findOne({ id: req.params.id }, (err, data) => { // find 1 car only based on id
+      if (data === null) { // check if mongoose model is inoperable or non-existent
+        data = localGallery.find(car => Number(req.params.id) === car.id); // get local module
+      }
       if (typeof data === 'object' && data !== null) { // check if data is an object and not empty
         res.send(data) // show the car
       } else { // else
