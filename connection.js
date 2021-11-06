b8fbe11d4ad074ac2679dbbab9de7288970e81d2
FesalBadday <fesal.n04@hotmail.com>
2021-11-06 15:29:14 -0600

    added comments

--- connection.js
+++ connection.js
@@ -1,18 +1,19 @@
 /***********************/
 /* Connect to Database */
 /***********************/
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const mongoose = require('mongoose') // import mongoose module
+const dotenv = require('dotenv').config() // import dotenv module
 
+// make the connection
 mongoose.connect(
   process.env.MONGODB_URL,
   { useUnifiedTopology: true, useNewUrlParser: true },
 )
-  .then(function () {
+  .then(function () { // check if connection is made
     console.log('Connected to DB...')
   })
-  .catch(function (err) {
+  .catch(function (err) { // catch errors
     console.log(err)
   });
 
-module.exports = mongoose;
+module.exports = mongoose // export mongoose
