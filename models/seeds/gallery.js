d92391119ab4c1bc7adb491e2c69eef7ad1d5ac6
FesalBadday <fesal.n04@hotmail.com>
2021-11-06 15:06:25 -0600

    initial commit

---
+++ models/seeds/gallery.js
@@ -0,0 +1,167 @@
+module.exports = [{
+  id: 1,
+  title: 'White Ford Mustang',
+  description: 'White Ford Mustang At Car Wash With Water Mist',
+  image: {
+    pathURL: 'assets/images/gal-car-1.jpg',
+    jpg: 'https://images.pexels.com/photos/3354648/pexels-photo-3354648.jpeg',
+    linkURL: 'https://www.pexels.com/photo/parked-white-coupe-during-night-3354648'
+  },
+  credit: 'Jan Kopřiva',
+  creditURL: 'https://www.pexels.com/@koprivakart',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 2,
+  title: 'Chevrolet Camaro',
+  description: 'White Chevrolet Camaro With High Speed Motion',
+  image: {
+    pathURL: 'assets/images/gal-car-2.jpg',
+    jpg: 'https://images.pexels.com/photos/1213294/pexels-photo-1213294.jpeg',
+    linkURL: 'https://www.pexels.com/photo/close-up-photography-of-white-chevrolet-camaro-1213294'
+  },
+  credit: 'Jesse Zheng',
+  creditURL: 'https://www.pexels.com/@jessef11',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 3,
+  title: 'Audi A5 Coupé',
+  description: 'Front Of A Shiny Yellow Audi A5 Coupé At Car Showroom',
+  image: {
+    pathURL: 'assets/images/gal-car-3.jpg',
+    jpg: 'https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg',
+    linkURL: 'https://www.pexels.com/photo/shiny-yellow-audi-car-in-showroom-1149831'
+  },
+  credit: 'Sourav Mishra',
+  creditURL: 'https://www.pexels.com/@photosbymishra',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 4,
+  title: 'Classic Car',
+  description: 'Old Sky Blue Classic Sedan With A Nice Sky Filter',
+  image: {
+    pathURL: 'assets/images/gal-car-4.jpg',
+    jpg: 'https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg',
+    linkURL: 'https://www.pexels.com/photo/blue-sedan-712618'
+  },
+  credit: 'neil kelly',
+  creditURL: 'https://www.pexels.com/@peely',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 5,
+  title: 'Audi R8',
+  description: 'Audi R8 Parked Under The Shadow Of Trees During Fall',
+  image: {
+    pathURL: 'assets/images/gal-car-5.jpg',
+    jpg: 'https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg',
+    linkURL: 'https://www.pexels.com/photo/photo-of-audi-parked-near-trees-1402787'
+  },
+  credit: 'Vlad Alexandru Popa',
+  creditURL: 'https://www.pexels.com/@vladalex94',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 6,
+  title: 'Ferrari F8',
+  description: 'Ferrari F8 Sports Car Parked On The Side Of The Road',
+  image: {
+    pathURL: 'assets/images/gal-car-6.jpg',
+    jpg: 'https://images.pexels.com/photos/2664399/pexels-photo-2664399.jpeg',
+    linkURL: 'https://www.pexels.com/photo/photo-of-ferrari-sports-car-parked-on-road-2664399'
+  },
+  credit: 'Roman Pohorecki',
+  creditURL: 'https://www.pexels.com/@romanp',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 7,
+  title: 'Nissan GT-R',
+  description: 'Nissan GT-R At Car Showroom With Spotlight Focus',
+  image: {
+    pathURL: 'assets/images/gal-car-7.jpg',
+    jpg: 'https://images.pexels.com/photos/2062551/pexels-photo-2062551.jpeg',
+    linkURL: 'https://www.pexels.com/photo/nissan-gt-r-on-car-show-inside-building-2062551'
+  },
+  credit: 'Orest Sv',
+  creditURL: 'https://www.pexels.com/@orestsv',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 8,
+  title: 'Dodge Challenger',
+  description: 'Stylish Dodge Challenger With Glowing Headlights',
+  image: {
+    pathURL: 'assets/images/gal-car-8.jpg',
+    jpg: 'https://images.pexels.com/photos/5214397/pexels-photo-5214397.jpeg',
+    linkURL: 'https://www.pexels.com/photo/stylish-automobile-with-glowing-headlights-on-parking-5214397'
+  },
+  credit: 'Erik Mclean',
+  creditURL: 'https://www.pexels.com/@introspectivedsgn',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 9,
+  title: 'Mercedes Benz',
+  description: 'White Mercedes Benz Sedan With An Epic Cloudy Weather',
+  image: {
+    pathURL: 'assets/images/gal-car-9.jpg',
+    jpg: 'https://images.pexels.com/photos/2611710/pexels-photo-2611710.jpeg',
+    linkURL: 'https://www.pexels.com/photo/white-mercedes-benz-sedan-2611710'
+  },
+  credit: 'Javier Aguilera',
+  creditURL: 'https://www.pexels.com/@jamphotography',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 10,
+  title: 'Red Ford Mustang',
+  description: 'Front Left Head Light Of A Red Ford Mustang With Spot Focus',
+  image: {
+    pathURL: 'assets/images/gal-car-10.jpg',
+    jpg: 'https://images.pexels.com/photos/638479/pexels-photo-638479.jpeg',
+    linkURL: 'https://www.pexels.com/photo/red-car-head-light-638479'
+  },
+  credit: 'Vraj Shah',
+  creditURL: 'https://www.pexels.com/@vraj-shah-115200',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 11,
+  title: 'Nissan GT-R Skyline',
+  description: 'Blue Nissan GT-R Skyline Parked Next To An Old Boat',
+  image: {
+    pathURL: 'assets/images/gal-car-11.jpg',
+    jpg: 'https://images.pexels.com/photos/9846137/pexels-photo-9846137.jpeg',
+    linkURL: 'https://www.pexels.com/photo/a-blue-car-parked-near-a-boat-9846137'
+  },
+  credit: 'Erik Mclean',
+  creditURL: 'https://www.pexels.com/@introspectivedsgn',
+  dateCreated: new Date().toISOString().split('T')[0]
+},
+
+{
+  id: 12,
+  title: 'Abandoned Truck',
+  description: 'Black And White Filter Of An Old Abandoned Truck',
+  image: {
+    pathURL: 'assets/images/gal-car-12.jpg',
+    jpg: 'https://images.pexels.com/photos/797570/pexels-photo-797570.jpeg',
+    linkURL: 'https://www.pexels.com/photo/single-cab-pickup-truck-797570'
+  },
+  credit: 'Tina Nord',
+  creditURL: 'https://www.pexels.com/@nord6',
+  dateCreated: new Date().toISOString().split('T')[0]
+}]
