63b44ead0fe07a66a0e6a559bbf55a50430f0455
FesalBadday <fesal.n04@hotmail.com>
2021-11-06 15:44:11 -0600

    added comments

--- routes/api.js
+++ routes/api.js
@@ -4,58 +4,40 @@
 const express = require('express') // import express module
 const router = express.Router()
 
-const mongoose = require('../connection')
-
+const Gallery = require('../models/gallery') // import gallery module
 const randomCar = require('./random-car') // import random-car module
-
-// gallery schema
-const gallerySchema = new mongoose.Schema({
-  id: Number,
-  title: String,
-  description: String,
-  image: Object,
-  pathURL: String,
-  jpg: String,
-  linkURL: String,
-  credit: String,
-  creditURL: String,
-  dateCreated: String
-})
-
-
-const Gallery = mongoose.model('gallery', gallerySchema)
 
 router.get('/cars', (req, res) => {
   try {
-    Gallery.find((err, data) => {
-      if (typeof data !== 'undefined' && Array.isArray(data)) {
-        if (req.query.filter === 'random') {
-          res.send(randomCar(data))
-        } else {
-          res.send(data)
+    Gallery.find((err, data) => { // find all cars
+      if (typeof data !== 'undefined' && Array.isArray(data)) { // check if data isn't undefined and it's an array
+        if (req.query.filter === 'random') { // check if filter = random 
+          res.send(randomCar(data)) // show the random car
+        } else { // else
+          res.send(data) // else show everything in the array
         }
       } else {
-        res.status(404).send({ error: '404 Not Found' })
+        res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
       }
     })
   } catch (err) {
-    console.log(err)
-    res.status(404).send({ error: '404 Not Found' })
+    console.log(err) // console log the error
+    res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
   }
 })
 
 router.get('/cars/:id', (req, res) => {
   try {
-    Gallery.findOne({ id: req.params.id }, (err, data) => {
-      if (typeof data === 'object' && data !== null) {
-        res.send(data)
-      } else {
-        res.status(404).send({ error: '404 Not Found' })
+    Gallery.findOne({ id: req.params.id }, (err, data) => { // find 1 car only based on id
+      if (typeof data === 'object' && data !== null) { // check if data is an object and not empty
+        res.send(data) // show the car
+      } else { // else
+        res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
       }
     })
   } catch (err) {
-    console.log(err)
-    res.status(404).send({ error: '404 Not Found' })
+    console.log(err) // console log the error
+    res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
   }
 })
 
