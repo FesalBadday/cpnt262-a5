02e1e272b83db0946dac0114c858b7867a5146ae
FesalBadday <fesal.n04@hotmail.com>
2021-11-08 15:18:54 -0700

    added async/await promises

--- routes/api.js
+++ routes/api.js
@@ -8,43 +8,51 @@ const Gallery = require('../models/gallery') // import gallery module
 const localGallery = require('../models/seeds/gallery') // import gallery module
 const randomCar = require('./random-car') // import random-car module
 
-router.get('/cars', (req, res) => {
+/**********/
+/* Routes */
+/**********/
+router.get('/cars', async (req, res) => {
   try {
-    Gallery.find((err, data) => { // find all cars
-      if (data.length === 0) { // check if mongoose model is inoperable or non-existent
-        data = localGallery // get local module
-      }
-      if (typeof data !== 'undefined' && Array.isArray(data)) { // check if data isn't undefined and it's an array
-        if (req.query.filter === 'random') { // check if filter = random 
-          res.send(randomCar(data)) // show the random car
-        } else { // else
-          res.send(data) // else show everything in the array
-        }
-      } else {
-        res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
-      }
-    })
+    // find all cars
+    let data = await Gallery.find()
+
+    // check if mongoose model is inoperable or non-existent
+    if (data.length === 0) {
+      data = localGallery // get local module
+    }
+
+    // check if filter = random
+    if (req.query.filter === 'random') {
+      res.send(randomCar(data)) // show the random car
+    } else { // else
+      res.send(data) // else show everything in the array
+    }
+
   } catch (err) {
     console.log(err) // console log the error
-    res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
+    res.send({ error: 'Gallery Not Found' }) // send JSON 404 error
   }
 })
 
-router.get('/cars/:id', (req, res) => {
+router.get('/cars/:id', async (req, res) => {
   try {
-    Gallery.findOne({ id: req.params.id }, (err, data) => { // find 1 car only based on id
-      if (data === null) { // check if mongoose model is inoperable or non-existent
-        data = localGallery.find(car => Number(req.params.id) === car.id) // get local module
-      }
-      if (typeof data === 'object' && data !== null) { // check if data is an object and not empty
-        res.send(data) // show the car
-      } else { // else
-        res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
-      }
-    })
+    // find 1 car only based on id
+    let data = await Gallery.findOne({ id: req.params.id })
+
+    // check if mongoose model is inoperable or non-existent
+    if (!data) {
+      data = localGallery.find(car => Number(req.params.id) === car.id) // get local module
+    }
+
+    if (data) { // check if mongoose data existest
+      res.send(data) // show the car
+    } else { // else
+      res.send({ error: 'Car Not Found' }) // send JSON 404 error
+    }
+
   } catch (err) {
     console.log(err) // console log the error
-    res.status(404).send({ error: '404 Not Found' }) // send JSON 404 error
+    res.send({ error: '404 Not Found' }) // send JSON 404 error
   }
 })
 
